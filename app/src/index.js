ce281eced9a202946dc0800c0f77017ad4648e1a
ajkraus04 <ajkraus04@gmail.com>
2023-08-26 15:05:30 -0400

    Added more boilerplate

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { ReactDOM } from "react";
-import { Root, createRoot } from "react-dom/client";
-import "./css/styles.css";
-import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./components/App";
+import React from 'react';
+import { ReactDOM } from 'react';
+import { Root, createRoot } from 'react-dom/client';
+import './css/styles.css';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './core/App';
 
-const element = document.getElementById("target");
+const element = document.getElementById('target');
 const root = createRoot(element);
 
 root.render(<App></App>);
