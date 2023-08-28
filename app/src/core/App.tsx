39e23e44e8bfd93ae6fce18dae944623ba217bc3
James Boswell <jamesboswell@Jamess-MacBook-Pro.local>
2023-08-28 10:01:00 -0400

    commit working test config less database calls

--- app/src/core/App.tsx
+++ app/src/core/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import TestingConfig from "../pages/config/Config";
 
 import { BrowserRouter, Routes, Router, Route } from "react-router-dom";
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <p className="bg-gray-500 text-center text-white">Hello World</p>
+      <TestingConfig></TestingConfig>
     </BrowserRouter>
   );
 };
