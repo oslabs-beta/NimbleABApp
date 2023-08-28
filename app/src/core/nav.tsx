0cae544774e953d672e25e20c92ee36c2d9e4446
James Boswell <jamesboswell@Jamess-MacBook-Pro.local>
2023-08-28 17:05:46 -0400

    integrated scaffolding

--- app/src/core/nav.tsx
+++ app/src/core/nav.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { pathNames } from '../constants/routes';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { pathNames } from "../constants/routes";
+import { useNavigate } from "react-router-dom";
 
 const Nav = (): React.JSX.Element => {
   return (
--- app/src/pages/home/home.tsx
+++ app/src/pages/home/home.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ExperimentCreate from "../../components/experimentCreate";
 import ActiveExperiment from "../../components/activeExperiments";
-
+import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 const Home = (): React.JSX.Element => {
   return (
     <div className="h-screen w-full bg-primary flex p-10 gap-2 font-mono">
       <ExperimentCreate></ExperimentCreate>
       <ActiveExperiment></ActiveExperiment>
+      <button>
+        <Link to="/config"> Configure new experiment</Link>
+      </button>
     </div>
   );
 };
