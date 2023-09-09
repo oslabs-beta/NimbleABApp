a99e8926291d2c5b8236461c19fb53b7f9eefe99
ajkraus04 <ajkraus04@gmail.com>
2023-09-09 11:30:09 -0400

    added instructions section

---
+++ app/src/components/instructions.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Instructions = (): React.JSX.Element => {
+ 
+
+  return (
+    <div className="mx-auto rounded-xl w-full h-60 bg-slate-800 text-white p-2 flex flex-col items-center font-mono mt-2">
+      <h1 className="text-lg">Instructions</h1>
+    </div>
+  );
+};
+
+export default Instructions;
